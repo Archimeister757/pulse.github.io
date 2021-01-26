9958a47dc14e307d0aed72b4f595cd35f0f0ca95
Artem <mohov.artem95@mail.ru>
2021-01-26 23:52:05 +0300

    add_map

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,41 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        speed: 1000,
-        adaptiveHeight: false,
+        speed: 500,
+        infinite: true,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 992,
                 settings: {
-                    dots: true,
-                    arrows: false
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '20px',
                 }
             }
         ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
 
